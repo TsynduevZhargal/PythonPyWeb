8c3ce3e1c33f58c17c78908f4d3ce4480d57f78e
zhacyn <tsynduev@mail.ru>
2024-03-28 21:04:37 +0300

    Lab 2

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Зарегистрируйте свои модели в админ панели здесь
 from django.contrib import admin
-from .models import Author
+from .models import Author, Tag, Entry, AuthorProfile
 
 admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Entry)
+admin.site.register(AuthorProfile)
--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -145,3 +145,27 @@ class Author(models.Model):
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+    def __str__(self):
+        return f"Автор: {self.author.username}; Стаж: {self.stage} лет"
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи",
+                            )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+    tags = models.ManyToManyField("Tag", related_name='entries')
+    def __str__(self):
+        return f"Автор: {self.author}; Тэги: {self.tags.all().values_list('name')}"
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название",
+                            )
+    def __str__(self):
+        return f"Название: {self.name}"
+
