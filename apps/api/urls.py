b402d3b55bd5d663173be495a7948e6c4a2e1f7c
Жаргал <tsynduev@mail.ru>
2024-04-17 01:17:00 +0300

    Lab 4 - 2.2

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,15 +1,11 @@
-from django.urls import path
-from .views import AuthorAPIView
-from .views import AuthorGenericAPIView
-from django.urls import include
-from .views import AuthorViewSet
+from django.urls import path, include
+from .views import AuthorAPIView, AuthorGenericAPIView, AuthorViewSet
 from rest_framework.routers import DefaultRouter
 
 #app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'authors_viewset', AuthorViewSet, basename='authors-viewset')
-
 
 urlpatterns = [
     path('authors/', AuthorAPIView.as_view(), name='author-list'),
